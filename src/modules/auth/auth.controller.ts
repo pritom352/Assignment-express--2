1b120769bda9f93a89abc7a172abfe2274433755
pritom352 <pritomproshad@gmail.com>
2026-05-21 12:01:53 +0600

    get jwt token on user login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -12,8 +12,20 @@ const signup = async(req:Request,res:Response)=>{
         res.status(500).json({ message: "Internal server error" });
     } }
 
+const login = async(req:Request,res:Response)=>{
+    try {
+        const result= await authService.loginUser(req.body.email as string,req.body.password as string)
 
+        console.log("Login result:", result); // Log the entire result object for debugging
+        const { password, ...userWithoutPassword } = result.user;
+        res.status(200).json({ message: "Login successful", user: userWithoutPassword, accessToken: result.accessToken });  
+    }catch (error:any) {
+     
+        res.status(500).json({success:false, message: error.message });
+    }
+}
 
 export const authController ={
-    signup
+    signup,
+    login
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import { pool } from "../../db";
 import type { IUser } from "../../types";
+import jwt from "jsonwebtoken";
+const jwtCode:string = "sitsldlfjsdotiweor"
+
 const createUser = async(payload:IUser)=>{
     const { name, email, password, role = "contributor" } = payload;
     const salt = bcrypt.genSaltSync(10);
@@ -14,7 +17,28 @@ const result = await pool.query(
 return result
 }
 
+const loginUser = async(email:string,password:string)=>{
+    const result = await pool.query(`SELECT * FROM users WHERE email = $1`,[email])
+if(result.rows.length===0){
+    throw new Error("No user found with this email")
+}
+const user= result.rows[0];
+const matchPassword = await bcrypt.compare(password,user.password);
+if(!matchPassword){
+    throw new Error("Invalid password")
+}
+//!  Genarate JWT token here and return it to the user
 
+const jwtPayload = {
+    id:user.id,
+    name:user.name,
+    role:user.role
+}
+const accessToken = jwt.sign(jwtPayload,jwtCode,{expiresIn:"10d"})
+return {accessToken , user}
+}
 export const authService ={
-    createUser
+    createUser,
+    loginUser,
+    jwtCode
 }
